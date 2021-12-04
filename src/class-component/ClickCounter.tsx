1a4c92bb247f5e1ffb39753d60fc140acd16e31f
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-12-04 09:29:41 +0900

    set counter component

---
+++ src/class-component/ClickCounter.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+interface Props {};
+
+interface State {
+  count: number;
+}
+
+export default class ClickCounter extends Component<Props, State> {
+  constructor(props: State) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  incrementCount = () => {
+    this.setState(prevState => {
+      return { count: prevState.count + 1};
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.incrementCount}>
+          clicled {this.state.count} times
+        </button>
+      </div>
+    )
+  }
+};
