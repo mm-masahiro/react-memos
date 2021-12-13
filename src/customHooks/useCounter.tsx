d8b44db58b6f08d0153134f3bbb3c815b6c02d38
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-12-13 23:18:26 +0900

    set types

--- src/customHooks/useCounter.tsx
+++ src/customHooks/useCounter.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
 export const useCounter = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
 
-  const increment = () => {
-    setCount((prevCount) => prevCount + 1);
+  const increment = (): void => {
+    setCount((prevCount): number => prevCount + 1);
   };
 
   return { current: count, increment: increment };
